3655a6ada8b132a48199259cfb72a11f253f74f1
krustylesponge <krustylesponge@gmail.com>
2025-10-09 00:22:35 -0400

    began building turrets
    turrets can currently target enemies, but cannot fire

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private Transform target;
+
+    private void OnDrawGizmosSelected() //so we can see the attack range
+    {
+        Handles.color = Color.yellow;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+}
