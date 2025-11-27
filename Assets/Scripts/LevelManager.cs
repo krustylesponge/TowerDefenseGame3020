1c7b68cbf8008ab80e6f3631d4043864e9330692
krustylesponge <krustylesponge@gmail.com>
2025-11-26 20:24:53 -0500

    began making currency system

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,8 +7,34 @@ public class LevelManager : MonoBehaviour
     public Transform startPoint; //so the enemies know where to spawn from
     public Transform[] path; //so the enemies know where to go
 
+    public int gold;
+
     private void Awake()
     {
         main = this;
+    }
+
+    private void Start()
+    {
+        gold = 100;
+    }
+
+    public void IncreaseGold(int amount)
+    {
+        gold += amount;
+    }
+
+    public bool SpendGold(int amount)
+    {
+        if (gold >= amount)
+        {
+            gold -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not enough gold");
+            return false;
+        }
     }
 }
