8ba5e0d317d3380e1d3847c6e7d9579c031cd4c3
krustylesponge <krustylesponge@gmail.com>
2025-12-03 23:53:39 -0500

    created an animation for opening and closing the shop

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,14 +4,18 @@ using TMPro;
 public class Menu : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI goldUI;
+    [SerializeField] Animator anim;
+
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        anim.SetBool("MenuOpen", isMenuOpen);
+    }
 
     private void OnGUI()
     {
         goldUI.text = "Gold: " + LevelManager.main.gold.ToString(); //makes the gold value able to be placed into the gold for the shop
-    }
-
-    public void SetSelected()
-    {
-
     }
 }
